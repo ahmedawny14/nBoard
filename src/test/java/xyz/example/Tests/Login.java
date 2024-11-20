42207e391aa82abfc701e5f19a6ba015e2aaa701
ahmedawny14 <109291582+ahmedawny14@users.noreply.github.com>
2024-11-21 01:04:30 +0200

    Update Login.java

--- src/test/java/xyz/example/Tests/Login.java
+++ src/test/java/xyz/example/Tests/Login.java
@@ -1,14 +1,15 @@
 package xyz.example.Tests;
 
 import data.ReadingFromExcel;
-import org.testng.annotations.DataProvider;
+ import org.openqa.selenium.WebElement;
+ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import xyz.example.Pages.HomePage;
 import xyz.example.Pages.LoginPage;
-
 import java.io.IOException;
-
+import java.time.Duration;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class LoginWithExcel extends TestBase {
 
@@ -25,34 +26,52 @@ public class LoginWithExcel extends TestBase {
 
 
 
-    @Test(dataProvider = "excelData")
-    public void loginWithValidData(String email,String password) throws InterruptedException {
+    @Test
+    public void loginWithValidData() throws InterruptedException, IOException {
         homePage = new HomePage(driver);
         loginPage = new LoginPage(driver);
-        Thread.sleep(3000);
+        ReadingFromExcel readingFromExcel = new ReadingFromExcel();
+        Object[][] excelData = readingFromExcel.getExcelData();
+        String email = (String) excelData[0][0];
+        String password = (String) excelData[0][1];
         loginPage.login(email, password);
-        Thread.sleep(3000);
-        homePage.signOut.click();
+        WebElement errorMessage = loginPage.loginErrorMessage;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        assertTrue(loginPage.background.isDisplayed());
+
+    }
+
+    @Test
+    public void loginWithNotExistedMail() throws InterruptedException, IOException {
+        homePage = new HomePage(driver);
+        loginPage = new LoginPage(driver);
+
+        ReadingFromExcel readingFromExcel = new ReadingFromExcel();
+        Object[][] excelData = readingFromExcel.getExcelData();
+        String email = (String) excelData[1][0];
+        String password = (String) excelData[1][1];
+        loginPage.login(email, password);
+        WebElement errorMessage = loginPage.loginErrorMessage;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        assertEquals(errorMessage.getText(), "Your credentials are not correct. Please check your email/password.");
 
 
     }
 
     @Test
-    public void loginWithNotExistedMail() throws InterruptedException {
+    public void loginWithWrongPassword() throws InterruptedException, IOException {
         homePage = new HomePage(driver);
         loginPage = new LoginPage(driver);
-        loginPage.login("ahmed1234@naqla.xyz", "ahmed1234");
-        assertEquals(loginPage.loginErrorMessage.getText(), "Your credentials are not correct. Please check your email/password.");
 
-    }
+        ReadingFromExcel readingFromExcel = new ReadingFromExcel();
+        Object[][] excelData = readingFromExcel.getExcelData();
+        String email = (String) excelData[2][0];
+        String password = (String) excelData[2][1];
+        loginPage.login(email, password);
+        WebElement errorMessage = loginPage.loginErrorMessage;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        assertEquals(errorMessage.getText(), "Your credentials are not correct. Please check your email/password.");
 
-    @Test
-    public void loginWithWrongPassword() throws InterruptedException {
-        homePage = new HomePage(driver);
-        loginPage = new LoginPage(driver);
-        loginPage.login("ahmedcrm10@naqla.xyz", "ahmed121314");
-        Thread.sleep(3000);
-        assertEquals(loginPage.loginErrorMessage.getText(), "Your credentials are not correct. Please check your email/password.");
 
     }
 
